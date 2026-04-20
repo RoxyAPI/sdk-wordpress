cc63f0f21661bb731dc04071ce6e6281358d77c1
Abhi <19635345+ph33nx@users.noreply.github.com>
2026-04-20 14:02:01 +0530

    chore: untrack build/ output, rebuilt in CI on release

--- build/blocks/dreams/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f2c0ac7b17eda5117af7');
--- build/blocks/tarot/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b2ebae7007c16b6129dc');
